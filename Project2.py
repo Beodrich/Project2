fd28083392cca6fb8d34864173881305b5846f0a
Beodrich <60353906+Beodrich@users.noreply.github.com>
2022-04-12 19:52:55 -0500

    hgfdhgrr

--- Project2.py
+++ Project2.py
@@ -1,58 +1,72 @@
+#Project 2 - Promotion Bias Simulation 
+#By: Ben Odrich and Emily Joyce
+
 import os
 import json
 import random
 import copy
-"""
-Rank1=["A":10, "B":10]
-[(5,5),(5,4)]
 
-SampleCo Output
-Rank 1A:7, B:3
-Rank 2A:8, B:2
-A:5, B:5
-A:6, B:4
-A:4, B:6
+#find 10 percent of each rank in order to find how many to demote or promote
+def FindTenPercent( biasList):
+        total= abs(sum(biasList)) #sum the values of each rank
+        return round((total*0.10)) #find 10% of the sum
 
-"""
-def FindTenPercent( companyList):
-        total= sum(companyList)
-     
-        return round((total*0.10))
-
-
+#promote top 10% people across ranks 
 def Promotion(companyList,numToPromote,rowToInsert,colToInsert):
-    
     if rowToInsert==0:#promotion can't happen in the first list
         return companyList
-    if rowToInsert<len(companyList) and colToInsert<len(companyList[0]):
+    if rowToInsert<len(companyList)-1 and colToInsert<len(companyList[0]):
         #insert a new number at the row and col
         #subtract one from pervious row and col
-        copyOfCompany=copy.deepcopy(companyList)
-        copyOfCompany[rowToInsert][colToInsert]+=numToPromote
-        copyOfCompany[rowToInsert+1][colToInsert]-=numToPromote
+        #causing a promotion to happen
+        copyOfCompany=copy.deepcopy(companyList) #copy company so to not overwrite it
+        copyOfCompany[rowToInsert+1][colToInsert]=max(copyOfCompany[rowToInsert+1][colToInsert]+numToPromote,0)#this is to prevent negative empolyess from happening
+        copyOfCompany[rowToInsert+1][colToInsert]=max(copyOfCompany[rowToInsert+1][colToInsert]-numToPromote,0)
         return copyOfCompany
+    return companyList
+
+#Demote botom 10% of people across ranks
 def DeMotion(companyList, numToPromote, rowToInsert,colToInsert):
-    if rowToInsert==len(companyList)-1:#only need to -1 if where doing zero based indexing 
-        return companyList#demotion can't happen at the last spot in the list, need to fire them to make this correct
-    if rowToInsert<len(companyList) and colToInsert<len(companyList[0]):
+    if rowToInsert==len(companyList)-1:
+        #"fire people at the bottom rank if demotion happens and replace the bottom ten percent with a uniformly random selection of people across the different categories.
+        index=0 #pos within each sublist
+        for i in range(len(companyList[rowToInsert])):
+            companyList[rowToInsert][index]=random.randrange(0,100)
+            index+=1 #increment pos by 1
+    if rowToInsert<len(companyList)-1 and colToInsert<len(companyList[0]):#this if statment demotes by one rank
         copyOfCompany=copy.deepcopy(companyList)
-        copyOfCompany[rowToInsert][colToInsert]-=numToPromote
-        copyOfCompany[rowToInsert+1][colToInsert]+=numToPromote
+        copyOfCompany[rowToInsert][colToInsert]= max(copyOfCompany[rowToInsert][colToInsert]-numToPromote,0)
+        copyOfCompany[rowToInsert+1][colToInsert]=max(copyOfCompany[rowToInsert+1][colToInsert]+numToPromote,0)
         return copyOfCompany
+    return companyList
+
+def Simulate(numGen, biasDict, companyList):
+    ranNum=0
+    for i in range(numGen):#run through x number of generations
+        biasList= ApplyBias(companyList,biasDict)#get a bias list
+        for j in range(len(biasList)):#go through the bias list
+            num=FindTenPercent(biasList[j])#figure out the ten percent
+            for value in range(num):#randomly promote or demote people based on the number generated 
+                ranNum=random.randrange(0,100)
+                if ranNum%2==0:
+                    companyList=Promotion(biasList,num,j,random.randrange(0,len(biasList[j])))
+                else:
+                    companyList=DeMotion(biasList,num,j,random.randrange(0,len(biasList[j])))
+
+    return companyList
     
 
+def OutPut(compayList, companyName):#output the list generated 
+    currentLetter='A'
+    print(companyName)
+    for i in range(len(companyList)):
+        currentLetter='A'
+        for j in range(len(companyList[i])):
+            print(f"{currentLetter}: {companyList[i][j]} ",end="")
+            currentLetter=chr(ord(currentLetter)+1)
+        print()
 
-
-    
-
-def Simulate(numGen, biasDict, compayList):
-    for i in range(numGen):
-        biasList= ApplyBias(companyList,biasDict)
-        for j in companyList:
-            #print(FindTenPercent(j))
-            return
-
-
+#appy bias generates a random bias between a set min and max
 def ApplyBias(companyList,biasDict):
     defaultMin=1
     defaultMax=100
@@ -64,58 +78,37 @@ def ApplyBias(companyList,biasDict):
     for i in range(len(companyList)):
         for j in range(len(companyList[i])):
         
-            biasMin=defaultMin-abs(biasDict[currentKey])
-            biasMax=defaultMax+biasDict[currentKey]
-            num=random.randrange(biasMin,biasMax)
-            temp.append(num)
-
-        
-            if index==size:
+            biasMin=defaultMin-abs(biasDict[currentKey])#generate the bias min
+            biasMax=defaultMax+biasDict[currentKey]#generate the bias max
+            num=random.randrange(biasMin,biasMax)#generate a random number between [biasmin,biasmax]
+            temp.append(num)#append a num to a temp list
+            
+            if index==size:#if index is == to size reset the index and append the temp list to the bias
                 index=0
                 currentKey='A'
                 biasList.append(copy.deepcopy(temp))
                 temp.clear()
 
-            else:
+            else:#incremenent the index and the key
                 index+=1
                 currentKey=chr(ord(currentKey)+1)
 
     return biasList
 
-
-
-
-
-
-   
-        
-
-         
-
-def readFile():
+def readFile(): #read in first 2 lines, assign the company name and number of generations to variables. Then read in bias, and then remaining lines
     companyList=[]
-    companyTxt=open("SampleCo.txt","r")
+    fileName= input("Please enter a file name: ")
+    companyTxt=open(fileName,"r")
     companyName=companyTxt.readline()
     numGen=companyTxt.readline()
 
     bias=json.loads(companyTxt.readline())
-
-    for x in companyTxt:
+    for x in companyTxt:#convert remaining lines of json to dictionary
         companyList.append(list(json.loads(x).values()))
-        
     companyTxt.close()
-    return (companyList,bias,companyName,numGen)
-
+    return (companyList,bias,companyName,numGen)#return a tuple of values 
 
 readFile()
 companyList,bias,name,gen=readFile()
-#print(ApplyBias(companyList,bias))
-#Simulate(int(gen),bias,companyList)
-d=Promotion(companyList,1,1,0)
-a=DeMotion(companyList,1,1,0)
-
-print(f"dict before promotion happens {companyList} ")
-print(f"dict after promotion happens {d}")
-print(f"dict after demotion happens {a}")
-
-#Promotion(companyList)
+companyList=Simulate(int(gen),bias,companyList)
+OutPut(companyList,name)
